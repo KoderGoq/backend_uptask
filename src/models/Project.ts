1796cd7617a3e4ed18d0363fc389754e47cbf8a6
Fernando Aguilar <100661177+KoderGoq@users.noreply.github.com>
2024-12-20 17:57:51 -0600

    Task Model Created

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,7 +1,7 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
-// Tipamos nuestro primer modelo (TypeScript)
-export type ProjectType = Document & {
+// Tipamos nuestra interfas
+export interface IProject extends Document {
   projectName: string;
   clientName: string;
   description: string;
@@ -26,5 +26,5 @@ const ProjectSchema: Schema = new Schema({
   }
 })
 
-const Project = mongoose.model<ProjectType>('Project', ProjectSchema); // Damos nombre unico a nuestro modelo para que salga en la base de datos, no puede existir otro igual.
+const Project = mongoose.model<IProject>('Project', ProjectSchema); // Damos nombre unico a nuestro modelo para que salga en la base de datos, no puede existir otro igual.
 export default Project;
