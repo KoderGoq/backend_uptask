83fa292e27b2b8554d719f76b04432bcbe576dc0
Fernando Aguilar <100661177+KoderGoq@users.noreply.github.com>
2024-12-20 16:31:43 -0600

    CRUD finished of Projects

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -1,4 +1,4 @@
-import type { Request, Response } from 'express'
+import type { Request, RequestHandler, Response } from 'express'
 import Project from '../models/Project';
 
 // Creamos nuestra clase para luego mandarla llamar en el router. 
@@ -22,6 +22,65 @@ export class ProjectController {
     try {
       const projects = await Project.find({});
       res.json(projects)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  // (Obtener proyectos por ID)
+  static getProjectByID: RequestHandler = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+      const project = await Project.findById(id);
+      if (!project) {
+        const error = new Error('Proyecto No encontrado')
+        res.status(404).json({ errror: error.message })
+        return;
+      }
+      res.json(project)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // (Actualizar proyectos por ID)
+  static updateProject: RequestHandler = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+      const project = await Project.findByIdAndUpdate(id, req.body); // Toma 2 parametros, uno es el ID y otro es lo que le pasemos para actualizar
+
+      if (!project) {
+        const error = new Error('Proyecto No encontrado')
+        res.status(404).json({ errror: error.message })
+        return;
+      }
+
+      await project.save();
+      res.send('Proyecto Actualizado Correctamente')
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  // (Eliminar proyectos por ID)
+  static deleteProject: RequestHandler = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+      const project = await Project.findById(id);
+
+      if (!project) {
+        const error = new Error('Proyecto No encontrado')
+        res.status(404).json({ errror: error.message })
+        return;
+      }
+
+      await project.deleteOne();
+      res.send('Proyecto Eliminado Correctamente');
+
     } catch (error) {
       console.log(error);
     }
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { body } from 'express-validator';
+import { body, param } from 'express-validator';
 import { ProjectController } from '../controllers/ProjectController';
 import { handleInputErrors } from '../middleware/validation';
 
@@ -8,6 +8,8 @@ const router = Router(); // Instanciamos nuestro router para ir generando el rou
 
 
 // Creamos el CRUD (Se hacen la peticion para que luego consulte al server.ts)
+
+// Crear proyecto
 router.post('/',
   body('projectName') // Agregamos validacion
     .trim().notEmpty().withMessage('El nombre del proyecto es obligatorio'),
@@ -18,6 +20,33 @@ router.post('/',
   handleInputErrors,
   ProjectController.createProject); // Mandamos el metodo del controlador
 
+
+// Obtener todos los proyectos
 router.get('/', ProjectController.getAllProjects)
+
+// Obtener Proyecto por ID
+router.get('/:id',
+  param('id').isMongoId().withMessage('ID No Valido'), // Validacion para el ID al momento de recuperar proyectos
+  handleInputErrors, // Middleware
+  ProjectController.getProjectByID)
+
+// Actualizar Proyecto
+router.put('/:id',
+  param('id').isMongoId().withMessage('ID No Valido'), // Validacion para el ID al momento de recuperar proyectos
+  body('projectName') // Agregamos validacion
+    .trim().notEmpty().withMessage('El nombre del proyecto es obligatorio'),
+  body('clientName')
+    .trim().notEmpty().withMessage('El nombre del cliente es obligatorio'),
+  body('description')
+    .trim().notEmpty().withMessage('La descripcion es obligatoria'),
+  handleInputErrors, // Middleware
+  ProjectController.updateProject)
+
+// Eliminar un proyecto por ID
+router.delete('/:id',
+  param('id').isMongoId().withMessage('ID No Valido'), // Validacion para el ID al momento de recuperar proyectos
+  handleInputErrors, // Middleware
+  ProjectController.deleteProject)
+
 
 export default router;
