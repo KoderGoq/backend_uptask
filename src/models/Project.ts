1b2502e5f1d1813e9580c5a3b7ae69163cf26406
Fernando Aguilar <100661177+KoderGoq@users.noreply.github.com>
2024-12-20 18:12:15 -0600

    Task Model Finished

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,10 +1,12 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Schema, Document, PopulatedDoc, Types } from 'mongoose';
+import { ITask } from './Task';
 
 // Tipamos nuestra interfas
 export interface IProject extends Document {
   projectName: string;
   clientName: string;
   description: string;
+  tasks: PopulatedDoc<ITask & Document>[];
 }
 
 // Creamos nuestro esquema de modelo (Mongoose)
@@ -23,8 +25,15 @@ const ProjectSchema: Schema = new Schema({
     type: String,
     require: true,
     trim: true
-  }
-})
+  },
+
+  tasks: [
+    {
+      type: Types.ObjectId,
+      ref: 'Task'
+    }
+  ]
+}, { timestamps: true })
 
 const Project = mongoose.model<IProject>('Project', ProjectSchema); // Damos nombre unico a nuestro modelo para que salga en la base de datos, no puede existir otro igual.
 export default Project;
--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,8 +1,22 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Schema, Document, Types } from 'mongoose';
+
+// Creando los status de las tareas
+const taskStatus = {
+  PENDING: 'pending',
+  ON_HOLD: 'onHold',
+  IN_PROGRESS: 'inProgress',
+  UNDER_REVIEW: 'underReview',
+  COMPLETED: 'completed'
+} as const
+
+// Leyendo solo los valores de las tareas
+export type TaskStatus = typeof taskStatus[keyof typeof taskStatus];
 
 export interface ITask extends Document {
   name: string;
   description: string;
+  project: Types.ObjectId;
+  status: TaskStatus;
 }
 
 export const TaskSchema: Schema = new Schema({
@@ -16,8 +30,19 @@ export const TaskSchema: Schema = new Schema({
     type: String,
     trim: true,
     require: true
+  },
+
+  project: {
+    type: Types.ObjectId,
+    ref: 'Project'
+  },
+
+  status: {
+    type: String,
+    enum: Object.values(taskStatus),
+    default: taskStatus.PENDING
   }
-})
+}, { timestamps: true })
 
 
 
