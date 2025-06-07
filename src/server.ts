62562def7b102c8db294b8b702ac730d46fe319b
Fernando Aguilar <100661177+KoderGoq@users.noreply.github.com>
2025-06-07 15:36:41 -0600

    morgan added

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import morgan from 'morgan';
 import { corsConfig } from './config/cors'; // Importamos la configuracion de CORS
 import { connectDB } from './config/db';
 import projecRoutes from './routes/projectRoutes'; // (router)
@@ -13,6 +14,9 @@ connectDB() // Mandamos llamar la conexion a la DB
 // Instanciamos express para inicializar nuestro backend
 const app = express();
 app.use(cors(corsConfig)) // Habilitamos CORS con la configuracion que tenemos en el archivo cors.ts
+
+// Login
+app.use(morgan('dev'))
 
 app.use(express.json()) // Habilitamos la lectura de JSON que mandemos en POST a la consola(req.body)
 
