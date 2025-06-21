bea0356890138f4555254c2cab250b93504ff5cc
Fernando Aguilar <100661177+KoderGoq@users.noreply.github.com>
2025-06-20 19:09:50 -0600

    updateProfiles & Changes Passwords

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -226,4 +226,47 @@ export class AuthController {
     res.json(req.user);
     return;
   }
+
+  static updateProfile = async (req: Request, res: Response) => {
+    const { name, email } = req.body;
+    const userExist = await User.findOne({ email });
+
+    if (userExist && userExist.id.toString() !== req.user.id.toString()) {
+      const error = new Error('Ese email ya esta registrado');
+      res.status(409).json({ error: error.message });
+      return;
+    }
+
+    req.user.name = name;
+    req.user.email = email;
+
+
+    try {
+      await req.user.save();
+      res.send('Perfil Actualizado Correctamente');
+    } catch (error) {
+      res.status(500).json('Error al actualizar perfil');
+    }
+  }
+
+  static changeUserPassword = async (req: Request, res: Response) => {
+    const { current_password, password } = req.body;
+
+    const user = await User.findById(req.user.id);
+
+    const isPasswordCorrect = await checkPassowrd(current_password, user.password);
+    if (!isPasswordCorrect) {
+      const error = new Error('El password actual es incorrecto');
+      res.status(401).json({ error: error.message });
+      return;
+    }
+
+    try {
+      user.password = await hashPassword(password);
+      await user.save();
+      res.send('El password se modifico correctamente')
+    } catch (error) {
+      res.status(500).json('Error al actualizar el password');
+    }
+  }
 }
